1a198a8771c5a78eb9060c366f827c574c6f1b9e
Kseniia Palchikova <kseniia.palchikova@gmail.com>
2021-03-17 23:25:43 +0100

    add eml-protection.js, edit main.js

--- resources/js/main.js
+++ resources/js/main.js
@@ -1,35 +1,3 @@
-// EMAIL PROTECTION ------------------------------------------------------------------
-const srcAdrs = '79180b0d100a0d57120a1c171018391e14181015571a1614';
-
-function decodeEmail(encodedString) {
-  let email = '';
-
-  const keyInHex = encodedString.substr(0, 2);
-
-  const key = parseInt(keyInHex, 16);
-
-  for (let n = 2; n < encodedString.length; n += 2) {
-    const charInHex = encodedString.substr(n, 2)
-    const char = parseInt(charInHex, 16);
-    const output = char ^ key;
-
-    email += String.fromCharCode(output);
-  }
-  return email;
-}
-
-let allElements = document.getElementsByClassName('eml-protected');
-
-for (let i = 0; i < allElements.length; i++) {
-  updateAnchor(allElements[i]);
-}
-
-function updateAnchor(element) {
-  const decoded = decodeEmail(srcAdrs);
-  element.textContent = decoded;
-  element.href = 'mailto:' + decoded;
-}
-
 // IMAGE VIEWER -------------------------------------------------------------------------
 const allImgLinks = document.querySelectorAll('.img-viewer-trigger');
 let currentImgIndex = '';
